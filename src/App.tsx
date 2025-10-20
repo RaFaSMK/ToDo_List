a598a8d6b281a60d8b1a29debc2aaf8850032352
RaFaSMK <rafael012chavess@gmail.com>
2025-10-20 09:34:50 -0300

    feat: Adiciona o componente skeleton e adequa o badge, button-con e input-checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import InputText from "./components/input-text"
 import InputCheckbox from "./components/input-checkbox"
 import Card from "./components/card"
 import Container from "./components/container"
+import Skeleton from "./components/skeleton"
 
 export default function App() {
 
@@ -38,9 +39,10 @@ export default function App() {
           <Icon svg={XIcon} />
         </div>
 
-        <div>
+        <div className="flex gap-1">
           <Badge variant="secondary">5</Badge>
           <Badge variant="primary">2 de 5</Badge>
+          <Badge loading variant="primary">5</Badge>
         </div>
 
         <div>
@@ -51,6 +53,7 @@ export default function App() {
           <ButtonIcon icon={TrashIcon} />
           <ButtonIcon icon={TrashIcon} variant="secondary" />
           <ButtonIcon icon={TrashIcon} variant="tertiary" />
+          <ButtonIcon icon={TrashIcon} loading />
         </div>
 
         <div>
@@ -64,6 +67,13 @@ export default function App() {
         <div>
           <Card size="md"> Olá mundo! </Card>
         </div>
+
+        <div className="space-y-2">
+          <Skeleton className="h-6" />
+          <Skeleton className="h-6" />
+          <Skeleton className="w-96 h-6" />
+        </div>
+
       </div >
     </Container>
   )
